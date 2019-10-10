0f515f2e465b90f4087a8172d2efc886fc44983a
shady <shady@dhcp-93-206.cu-internet.carleton.ca>
2019-10-10 16:54:08 -0400

    first commit

---
+++ MyAppsysc3010 copy/app/src/main/java/com/example/myappsysc3010/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myappsysc3010;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    fun sendMessage(view: View) {
+        // Do something in response to button
+    }
+}
